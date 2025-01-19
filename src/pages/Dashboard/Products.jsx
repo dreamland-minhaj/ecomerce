968e3bf1815bdd6a025a7454a52b0b57d56cb321
dreamland-minhaj <dreamland.minhaj@gmail.com>
2025-01-20 01:09:50 +0600

    design fixed product

--- src/pages/Dashboard/Products.jsx
+++ src/pages/Dashboard/Products.jsx
@@ -387,7 +387,7 @@ const Products = () => {
     type="submit"
     className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
   >
-    Create Product
+    Update Product
   </button>
         </form>
       </Modal>
